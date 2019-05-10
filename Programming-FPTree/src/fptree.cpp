ab8d92e8496ac42810a4334b6b171fed26b651d1
hunk954 <415391519@qq.com>
2019-05-10 20:50:10 +0800

    InnerNoede::findIndex()

--- Programming-FPTree/src/fptree.cpp
+++ Programming-FPTree/src/fptree.cpp
@@ -28,15 +28,15 @@ int InnerNode::findIndex(const Key& k) {
     // done
     //k0:c0&c1, k1:c2, k2:c3, ... kn:cn+1
     int low = 0, high = nKeys - 1, mid;
-    while(low <= high){
+    while(low < high){
         mid = (low + high) / 2;
-        if (keys[mid] == k) {
+        if (k == keys[mid]) 
             return mid + 1;
-        } else if (keys[mid] > k) {
-            high = mid - 1;
-        } else {
+        else if (k < keys[mid]) 
+            high = mid;
+        else 
             low = mid + 1;
-        }
+        
     }
     return low;
 }
@@ -53,7 +53,7 @@ void InnerNode::insertNonFull(const Key& k, Node* const& node) {
     int i;
     for (i = nKeys; i > index; i--)
         keys[i] = keys[i - 1];
-    key[index] = k;
+    keys[index] = k;
     nKeys++;
     for (i = nChild; i > index + 1; i--)
         childrens[i] = childrens[i - 1];
