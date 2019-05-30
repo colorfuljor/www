bb838c1b347a793c00db71f8a74dacc4112d4147
colorfuljor <913864870@qq.com>
2019-05-30 20:32:55 +0800

    Update ycsb.cpp
    测试

--- Programming-FPTree/src/ycsb.cpp
+++ Programming-FPTree/src/ycsb.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 const string workload = "../workloads/"; // TODO: the workload folder filepath
 
-const string load = workload + "1w-rw-50-50-load.txt"; // TODO: the workload_load filename
-const string run  = workload + "1w-rw-50-50-run.txt"; // TODO: the workload_run filename
+const string load = workload + "220w-rw-50-50-load.txt"; // TODO: the workload_load filename
+const string run  = workload + "220w-rw-50-50-run.txt"; // TODO: the workload_run filename
 
-const int READ_WRITE_NUM = 10000; // TODO: amount of operations
+const int READ_WRITE_NUM = 2200000; // TODO: amount of operations
 
 int main()
 {        
