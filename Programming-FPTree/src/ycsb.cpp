f63c03d6b7f9f496de3ed00b527f952259f2bc85
hunk954 <415391519@qq.com>
2019-05-30 18:41:03 +0800

    Update ycsb.cpp
    将lyscb的leveldb相关内容拷贝到yscb

--- Programming-FPTree/src/ycsb.cpp
+++ Programming-FPTree/src/ycsb.cpp
@@ -6,12 +6,12 @@
 #define VALUE_LEN 8
 using namespace std;
 
-const string workload = ""; // TODO: the workload folder filepath
+const string workload = "../workloads/"; // TODO: the workload folder filepath
 
-const string load = workload + ""; // TODO: the workload_load filename
-const string run  = workload + ""; // TODO: the workload_run filename
+const string load = workload + "1w-rw-50-50-load.txt"; // TODO: the workload_load filename
+const string run  = workload + "1w-rw-50-50-run.txt"; // TODO: the workload_run filename
 
-const int READ_WRITE_NUM = 0; // TODO: amount of operations
+const int READ_WRITE_NUM = 10000; // TODO: amount of operations
 
 int main()
 {        
@@ -65,11 +65,44 @@ int main()
     leveldb::Options options;
     leveldb::WriteOptions write_options;
     // TODO: initial the levelDB
+    // DONE
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+    assert(status.ok());
+
     inserted = 0;
+    queried = 0;
+    t = 0;
     printf("Load phase begins \n");
     // TODO: read the ycsb_read
+    // DONE
+    ycsb_load = (FILE*)malloc(sizeof(FILE));
+    if ((ycsb_load = fopen(load.c_str(), "r")) == NULL) {
+        printf("cannot open file!\n");
+        return -1;
+    }
+
+    buf = new char[32];
+    while (fgets(buf,32,ycsb_load) != NULL)  {
+        len = strlen(buf);
+        buf[len] = '\0';
+       	op[t] = 0;
+        key[t] = atoll(buf + 7);
+        t++;
+    }
+
     clock_gettime(CLOCK_MONOTONIC, &start);
     // TODO: load the levelDB
+    // DONE
+    int i;
+    for (i = 0; i < READ_WRITE_NUM; i++) {
+        if (op[t] == 0) {
+            status = db->Put(write_options, to_string(key[i]), to_string(key[i]));
+            assert(status.ok());
+            inserted++;
+        }
+    }
+    
     clock_gettime(CLOCK_MONOTONIC, &finish);
 	single_time = (finish.tv_sec - start.tv_sec) * 1000000000.0 + (finish.tv_nsec - start.tv_nsec);
     printf("Load phase finishes: %d items are inserted \n", inserted);
@@ -80,11 +113,60 @@ int main()
 	operation_num = 0;
     inserted = 0;		
     // TODO: read the ycsb_run
-
+    // DONE
+    if ((ycsb_run = fopen(run.c_str(), "r")) == NULL) {
+        printf("cannot open file!\n");
+        return -1;
+    }
+    t = 0;
+    while (fgets(buf,32,ycsb_run) != NULL)  {
+        len = strlen(buf);
+        buf[len] = '\0';
+        switch(buf[0]){
+			case 'I': //insert
+				op[t] = 0;
+				key[t] = atoll(buf + 7);
+				break;
+			case 'D': //delete
+				op[t] = 1;
+				key[t] = atoll(buf + 7);
+				break;
+			case 'U': //update
+				op[t] = 2;
+				key[t] = atoll(buf + 7);
+				break;
+			case 'R': //read
+				op[t] = 3;
+				key[t] = atoll(buf + 5);
+				break;
+		}
+        operation_num++;
+        t++; 
+    }
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // TODO: run the workload_run in levelDB
-
+    // DONE
+     for (i = 0; i < operation_num; i++) {
+    	//增其实就是改 因为key == value 
+        if (op[i] == 0 && op[i] == 2) {
+            status = db->Put(write_options, to_string(key[i]), to_string(key[i]));
+            assert(status.ok());
+            inserted++;
+        }
+        else if(op[i] == 1){
+		    // 删除key
+		    status = db->Delete(leveldb::WriteOptions(), to_string(key[i]));
+		    assert(status.ok());
+		}
+        else if(op[i] == 3){
+            string val;
+            status = db->Get(leveldb::ReadOptions(), to_string(key[i]), &val);
+            assert(status.ok());
+            queried++;
+        }
+		
+    }
 	clock_gettime(CLOCK_MONOTONIC, &finish);
     fclose(ycsb_read);
 	single_time = (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
